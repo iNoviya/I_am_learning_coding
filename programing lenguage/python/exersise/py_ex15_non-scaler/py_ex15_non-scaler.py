64cd8e67b14b010b072522ade81115f5a23c021c
iNoviya <75850975+iNoviya@users.noreply.github.com>
2023-10-23 19:18:17 +0300

    py_ex15-16

---
+++ programing lenguage/python/exersise/py_ex15_non-scaler/py_ex15_non-scaler.py
@@ -0,0 +1,8 @@
+score = [25,49,98,78]
+total = 0
+lengh = len(score)
+for i in score:
+   total +=i
+average = total / lengh
+print("total score is :",total,"\nnumber of people :",lengh,"\naritmatic avarege is :",average)
+
---
+++ programing lenguage/python/exersise/py_ex16_split-join/py_ex16_split-join.py
@@ -0,0 +1,20 @@
+list = "hello word"
+split = list.split(" ")
+print(split)
+
+list2 = "This course is so coll. So, I am so happy"
+split2 = list2.split()
+print(split2)
+
+list3 = ["this","is","a","list","Do","you","belive","it?"]
+join1 = "  ".join(list3)
+print(join1)
+
+# split2 to list2  reverse transform  list to string
+join2 = " ".join(split2)
+print(join2)
+
+peopleInfo = {
+   1: {"arda", 236},
+   2: {"selin", 535}
+}
