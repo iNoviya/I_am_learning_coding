14f0fe0644623485b236556c22675e74c510df7f
iNoviya <75850975+iNoviya@users.noreply.github.com>
2024-03-10 23:01:17 +0300

    java_ex5_ternary-operator

---
+++ programing lenguage/java/exersize/java_ex5_ternary-operator/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ternary operator");
+
+
+        int a = 5, b = 9;
+        int result = a + b ;
+        String y = result >= 10 ?  "true" :  "false" ;
+        System.out.println(y);
+
+        y = result == 10 ?  "true" :  "false" ;
+        System.out.println(y);
+    }
+}
